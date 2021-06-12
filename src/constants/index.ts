20d7b2baf0fe39adbcee524e4ca356517f4f99f8
Pedro Medina <pedromedina190@gmail.com>
2021-06-12 19:28:57 -0400

    initial

---
+++ src/constants/index.ts
@@ -0,0 +1,263 @@
+import { ChainId, JSBI, Percent, Token, WAVAX } from '@partyswap-libs/sdk'
+import { AbstractConnector } from '@web3-react/abstract-connector'
+
+import { injected } from '../connectors'
+
+export const GAS_PRICE = 225
+
+// export const ROUTER_ADDRESS: { [chainId in ChainId]: string } = {
+//   [ChainId.FUJI]: '0x2D99ABD9008Dc933ff5c0CD271B88309593aB921',
+//   [ChainId.AVALANCHE]: '0xE54Ca86531e17Ef3616d22Ca28b0D458b6C89106'
+// }
+
+export const ROUTER_ADDRESS = {
+  [ChainId.AVALANCHE]: '0xb0efe42a4C70105aBBCB1cEBDd737BD03f51496D',
+  [ChainId.FUJI]: '0xDf822413C6C87962D85017c0Df4d8ff422f1bfEC'
+}
+
+export const LANDING_PAGE = 'https://pangolin.exchange/'
+export const ANALYTICS_PAGE = 'https://info.pangolin.exchange/'
+
+export const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000'
+
+export const GOVERNANCE_ADDRESS = '0xb0Ff2b1047d9E8d294c2eD798faE3fA817F43Ee1'
+
+// a list of tokens by chain
+type ChainTokenList = {
+  readonly [chainId in ChainId]: Token[]
+}
+
+export const PNG: { [chainId in ChainId]: Token } = {
+  [ChainId.FUJI]: new Token(ChainId.FUJI, '0x20C62EEde571409f7101076F8dA0221867AA46dc', 18, 'PNG', 'Pangolin'),
+  [ChainId.AVALANCHE]: new Token(ChainId.AVALANCHE, '0x60781C2586D68229fde47564546784ab3fACA982', 18, 'PNG', 'Pangolin')
+}
+
+export const ETH: { [chainId in ChainId]: Token } = {
+  [ChainId.FUJI]: new Token(ChainId.FUJI, ZERO_ADDRESS, 18, 'ETH', 'Ether'),
+  [ChainId.AVALANCHE]: new Token(ChainId.AVALANCHE, '0xf20d962a6c8f70c731bd838a3a388D7d48fA6e15', 18, 'ETH', 'Ether')
+}
+
+export const USDT: { [chainId in ChainId]: Token } = {
+  [ChainId.FUJI]: new Token(ChainId.FUJI, ZERO_ADDRESS, 6, 'USDT', 'Tether USD'),
+  [ChainId.AVALANCHE]: new Token(
+    ChainId.AVALANCHE,
+    '0xde3A24028580884448a5397872046a019649b084',
+    6,
+    'USDT',
+    'Tether USD'
+  )
+}
+
+export const WBTC: { [chainId in ChainId]: Token } = {
+  [ChainId.FUJI]: new Token(ChainId.FUJI, ZERO_ADDRESS, 8, 'WBTC', 'Wrapped Bitcoin'),
+  [ChainId.AVALANCHE]: new Token(
+    ChainId.AVALANCHE,
+    '0x408D4cD0ADb7ceBd1F1A1C33A0Ba2098E1295bAB',
+    8,
+    'WBTC',
+    'Wrapped Bitcoin'
+  )
+}
+
+export const LINK: { [chainId in ChainId]: Token } = {
+  [ChainId.FUJI]: new Token(ChainId.FUJI, ZERO_ADDRESS, 18, 'LINK', 'ChainLink Token'),
+  [ChainId.AVALANCHE]: new Token(
+    ChainId.AVALANCHE,
+    '0xB3fe5374F67D7a22886A0eE082b2E2f9d2651651',
+    18,
+    'LINK',
+    'ChainLink Token'
+  )
+}
+
+export const DAI: { [chainId in ChainId]: Token } = {
+  [ChainId.FUJI]: new Token(ChainId.FUJI, '0x01a38eCc037DCf490045f6E4D74296F212331B99', 18, 'DAI', 'Dai Stablecoin'),
+  [ChainId.AVALANCHE]: new Token(
+    ChainId.AVALANCHE,
+    '0xbA7dEebBFC5fA1100Fb055a87773e1E99Cd3507a',
+    18,
+    'DAI',
+    'Dai Stablecoin'
+  )
+}
+
+export const UNI: { [chainId in ChainId]: Token } = {
+  [ChainId.FUJI]: new Token(ChainId.FUJI, ZERO_ADDRESS, 18, 'UNI', 'Uniswap'),
+  [ChainId.AVALANCHE]: new Token(ChainId.AVALANCHE, '0xf39f9671906d8630812f9d9863bBEf5D523c84Ab', 18, 'UNI', 'Uniswap')
+}
+
+export const SUSHI: { [chainId in ChainId]: Token } = {
+  [ChainId.FUJI]: new Token(ChainId.FUJI, ZERO_ADDRESS, 18, 'SUSHI', 'SushiToken'),
+  [ChainId.AVALANCHE]: new Token(
+    ChainId.AVALANCHE,
+    '0x39cf1BD5f15fb22eC3D9Ff86b0727aFc203427cc',
+    18,
+    'SUSHI',
+    'SushiToken'
+  )
+}
+
+export const AAVE: { [chainId in ChainId]: Token } = {
+  [ChainId.FUJI]: new Token(ChainId.FUJI, ZERO_ADDRESS, 18, 'AAVE', 'Aave Token'),
+  [ChainId.AVALANCHE]: new Token(
+    ChainId.AVALANCHE,
+    '0x8cE2Dee54bB9921a2AE0A63dBb2DF8eD88B91dD9',
+    18,
+    'AAVE',
+    'Aave Token'
+  )
+}
+
+export const YFI: { [chainId in ChainId]: Token } = {
+  [ChainId.FUJI]: new Token(ChainId.FUJI, ZERO_ADDRESS, 18, 'YFI', 'yearn.finance'),
+  [ChainId.AVALANCHE]: new Token(
+    ChainId.AVALANCHE,
+    '0x99519AcB025a0e0d44c3875A4BbF03af65933627',
+    18,
+    'YFI',
+    'yearn.finance'
+  )
+}
+
+export const SNOB: { [chainId in ChainId]: Token } = {
+  [ChainId.FUJI]: new Token(ChainId.FUJI, '0xf319e2f610462f846d6e93f51cdc862eeff2a554', 18, 'SNOB', 'Snowball'),
+  [ChainId.AVALANCHE]: new Token(
+    ChainId.AVALANCHE,
+    '0xc38f41a296a4493ff429f1238e030924a1542e50',
+    18,
+    'SNOB',
+    'Snowball'
+  )
+}
+
+export const VSO: { [chainId in ChainId]: Token } = {
+  [ChainId.FUJI]: new Token(ChainId.FUJI, ZERO_ADDRESS, 18, 'VSO', 'VersoToken'),
+  [ChainId.AVALANCHE]: new Token(
+    ChainId.AVALANCHE,
+    '0x846D50248BAf8b7ceAA9d9B53BFd12d7D7FBB25a',
+    18,
+    'VSO',
+    'VersoToken'
+  )
+}
+
+export const AIRDROP_ADDRESS: { [chainId in ChainId]?: string } = {
+  [ChainId.FUJI]: ZERO_ADDRESS,
+  [ChainId.AVALANCHE]: '0x0C58C2041da4CfCcF5818Bbe3b66DBC23B3902d9'
+}
+
+const WAVAX_ONLY: ChainTokenList = {
+  [ChainId.FUJI]: [WAVAX[ChainId.FUJI]],
+  [ChainId.AVALANCHE]: [WAVAX[ChainId.AVALANCHE]]
+}
+
+// used to construct intermediary pairs for trading
+export const BASES_TO_CHECK_TRADES_AGAINST: ChainTokenList = {
+  ...WAVAX_ONLY,
+  [ChainId.FUJI]: [...WAVAX_ONLY[ChainId.FUJI]]
+}
+
+/**
+ * Some tokens can only be swapped via certain pairs, so we override the list of bases that are considered for these
+ * tokens.
+ */
+export const CUSTOM_BASES: { [chainId in ChainId]?: { [tokenAddress: string]: Token[] } } = {
+  [ChainId.FUJI]: {}
+}
+
+// used for display in the default list when adding liquidity
+export const SUGGESTED_BASES: ChainTokenList = {
+  ...WAVAX_ONLY,
+  [ChainId.FUJI]: [...WAVAX_ONLY[ChainId.FUJI]]
+}
+
+// used to construct the list of all pairs we consider by default in the frontend
+export const BASES_TO_TRACK_LIQUIDITY_FOR: ChainTokenList = {
+  ...WAVAX_ONLY,
+  [ChainId.FUJI]: [...WAVAX_ONLY[ChainId.FUJI]]
+}
+
+export const PINNED_PAIRS: { readonly [chainId in ChainId]?: [Token, Token][] } = {
+  [ChainId.AVALANCHE]: []
+}
+
+export interface WalletInfo {
+  connector?: AbstractConnector
+  name: string
+  iconName: string
+  description: string
+  href: string | null
+  color: string
+  primary?: true
+  mobile?: true
+  mobileOnly?: true
+}
+
+export const SUPPORTED_WALLETS: { [key: string]: WalletInfo } = {
+  INJECTED: {
+    connector: injected,
+    name: 'Injected',
+    iconName: 'arrow-right.svg',
+    description: 'Injected web3 provider.',
+    href: null,
+    color: '#010101',
+    primary: true
+  },
+  METAMASK: {
+    connector: injected,
+    name: 'MetaMask',
+    iconName: 'metamask.png',
+    description: 'Easy-to-use browser extension.',
+    href: null,
+    color: '#E8831D'
+  }
+}
+
+export const NetworkContextName = 'NETWORK'
+
+// export const AVALANCHE_CHAIN_PARAMS = {
+//   chainId: '0xa86a', // A 0x-prefixed hexadecimal chainId
+//   chainName: 'Avalanche Mainnet C-Chain',
+//   nativeCurrency: {
+//     name: 'Avalanche',
+//     symbol: 'AVAX',
+//     decimals: 18
+//   },
+//   rpcUrls: ['https://api.avax.network/ext/bc/C/rpc'],
+//   blockExplorerUrls: ['https://cchain.explorer.avax.network/']
+// }
+
+export const AVALANCHE_CHAIN_PARAMS = {
+  chainId: '0xa869', // A 0x-prefixed hexadecimal chainId
+  chainName: 'Avalanche Fuji C-Chain',
+  nativeCurrency: {
+    name: 'Avalanche',
+    symbol: 'AVAX',
+    decimals: 18
+  },
+  rpcUrls: ['https://api.avax-test.network/ext/bc/C/rpc'],
+  blockExplorerUrls: ['https://cchain.explorer.avax-test.network/']
+}
+
+// default allowed slippage, in bips
+export const INITIAL_ALLOWED_SLIPPAGE = 50
+// 60 minutes, denominated in seconds
+export const DEFAULT_DEADLINE_FROM_NOW = 60 * 60
+
+export const BIG_INT_ZERO = JSBI.BigInt(0)
+
+// one basis point
+export const ONE_BIPS = new Percent(JSBI.BigInt(1), JSBI.BigInt(10000))
+export const BIPS_BASE = JSBI.BigInt(10000)
+// used for warning states
+export const ALLOWED_PRICE_IMPACT_LOW: Percent = new Percent(JSBI.BigInt(100), BIPS_BASE) // 1%
+export const ALLOWED_PRICE_IMPACT_MEDIUM: Percent = new Percent(JSBI.BigInt(300), BIPS_BASE) // 3%
+export const ALLOWED_PRICE_IMPACT_HIGH: Percent = new Percent(JSBI.BigInt(500), BIPS_BASE) // 5%
+// if the price slippage exceeds this number, force the user to type 'confirm' to execute
+export const PRICE_IMPACT_WITHOUT_FEE_CONFIRM_MIN: Percent = new Percent(JSBI.BigInt(1000), BIPS_BASE) // 10%
+// for non expert mode disable swaps above this
+export const BLOCKED_PRICE_IMPACT_NON_EXPERT: Percent = new Percent(JSBI.BigInt(1500), BIPS_BASE) // 15%
+
+// used to ensure the user doesn't send so much ETH so they end up with <.01
+export const MIN_ETH: JSBI = JSBI.exponentiate(JSBI.BigInt(10), JSBI.BigInt(16)) // .01 ETH
+export const BETTER_TRADE_LINK_THRESHOLD = new Percent(JSBI.BigInt(75), JSBI.BigInt(10000))
