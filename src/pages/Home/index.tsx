722357cdd6cd44ae4d7d6b5c456c8ffe5f39f90a
Brandon Hernandez <hernandezbrandon01@gmail.com>
2021-08-09 21:02:54 -0400

    The homepage now supports dark mode

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,40 +2,68 @@ import React from 'react'
 import styled from 'styled-components'
 import bonnie from '../../assets/svg/home-hero-bonnie.svg'
 import trent from '../../assets/svg/home-hero-trent.svg'
-import iconFarms from '../../assets/images/grid-item-header-farms-staking.png'
-import iconStats from '../../assets/images/grid-item-header-stats.png'
+import iconFarms from '../../assets/svg/grid-item-header-farms-staking.svg'
+import iconStats from '../../assets/svg/grid-item-header-stats.svg'
 import bannerBackground from '../../assets/svg/home-banner-background.svg'
 import bannerPipo from '../../assets/svg/home-banner-pipo.svg'
 import BannerPiñata1 from '../../assets/svg/home-banner-piñata-1.svg'
 import BannerPiñata2 from '../../assets/svg/home-banner-piñata-2.svg'
 import BannerPiñata3 from '../../assets/svg/home-banner-piñata-3.svg'
+import { useIsDarkMode } from '../../state/user/hooks'
 
 const Wrapper = styled.div`
   width: 100vw;
   margin-top: -2rem;
+  background-color: ${({ theme }) => theme.surface3};
   @media (min-width: 720px) {
     margin-top: -100px;
   }
 `
 
+const Hero = styled.div`
+  background: ${({ theme }) => theme.gradient1};
+`
+
+const GridItem = styled.div`
+  background-color: ${({ theme }) => theme.surface4};
+  .grid-item-header h4,
+  .grid-item-header p {
+    color: ${({ theme }) => theme.text1};
+  }
+  .grid-item-header p {
+    opacity: 0.8;
+  }
+  .grid-item-stats span,
+  .grid-item-farms p:nth-child(1) {
+    color: ${({ theme }) => theme.text6};
+  }
+  &.grid-banner {
+    background-color: ${({ theme }) => theme.surface4};
+    background: ${({ theme }) => theme.gradient2};
+  }
+`
+
 export default function Home() {
+  const isDarkMode = useIsDarkMode()
   return (
     <Wrapper>
-      <div className="hero">
+      <Hero className="hero">
         <div className="hero-container">
           <div className="hero-content">
             <p className="smallText">Welcome to the Party</p>
-            <h1>The most reliable Avalanche swap yet</h1>
+            <h1>The most reliable Avalanchche swap yet</h1>
             <button className="btn hero-btn">Unlock Wallet</button>
           </div>
         </div>
-        <img src={bonnie} alt="Bonnie" className="hero-img hero-img-bonnie" />
+
+        <img src={isDarkMode ? bonnie : bonnie} alt="Bonnie" className="hero-img hero-img-bonnie" />
+
         <img src={trent} alt="Trent" className="hero-img hero-img-trent" />
-      </div>
+      </Hero>
 
       <div className="grid">
         <div className="grid-container">
-          <div className="grid-item">
+          <GridItem className="grid-item">
             <div className="grid-item-header">
               <img src={iconFarms} alt="Farms & Staking icon" />
               <div>
@@ -58,8 +86,8 @@ export default function Home() {
             <p>
               <button className="btn">Claim All</button>
             </p>
-          </div>
-          <div className="grid-item">
+          </GridItem>
+          <GridItem className="grid-item">
             <div className="grid-item-header">
               <img src={iconStats} alt="Stats and analytics icon" />
               <div>
@@ -81,8 +109,8 @@ export default function Home() {
             <p>
               <button className="btn">Check Analytics</button>
             </p>
-          </div>
-          <div className="grid-item grid-banner">
+          </GridItem>
+          <GridItem className="grid-item grid-banner">
             <div className="grid-banner-content">
               <p className="smallText">Stake and earn</p>
               <h2 className="h1">
@@ -97,7 +125,7 @@ export default function Home() {
             <img src={BannerPiñata1} alt="Banner Piñata" className="grid-banner-1" />
             <img src={BannerPiñata2} alt="Banner Piñata" className="grid-banner-2" />
             <img src={BannerPiñata3} alt="Banner Piñata" className="grid-banner-3" />
-          </div>
+          </GridItem>
         </div>
       </div>
     </Wrapper>
