e7bbf25697a170c4c5cc7f812a710af1f3664d74
Pedro Medina <pedromedina190@gmail.com>
2021-09-14 16:03:52 -0300

    repair lists

--- src/constants/lists.ts
+++ src/constants/lists.ts
@@ -1,17 +1,19 @@
 // the Party Default token list lives here
-export const AB_TOKEN_LIST_URL = 'https://raw.githubusercontent.com/pangolindex/tokenlists/main/ab.tokenlist.json'
 export const FUJI_TOKEN_LIST_URL = 'https://raw.githubusercontent.com/PartySwapDEX/token-lists/main/fuji.tokenlist.json'
-export const DEFAULT_TOKEN_LIST_URL = 'https://raw.githubusercontent.com/pangolindex/tokenlists/main/aeb.tokenlist.json'
+export const AEB_TOKENLIST = 'https://raw.githubusercontent.com/pangolindex/tokenlists/main/aeb.tokenlist.json'
 export const TOP_15_TOKEN_List = 'https://raw.githubusercontent.com/pangolindex/tokenlists/main/top15.tokenlist.json'
 export const DEFI_TOKEN_LIST = 'https://raw.githubusercontent.com/pangolindex/tokenlists/main/defi.tokenlist.json'
 export const STABLECOIN_TOKEN_LIST =
   'https://raw.githubusercontent.com/pangolindex/tokenlists/main/stablecoin.tokenlist.json'
+export const AVAX_BRIDGE_LIST = 'https://raw.githubusercontent.com/pangolindex/tokenlists/main/ab.tokenlist.json'
+export const DEFAULT_TOKEN_LIST_URL = AVAX_BRIDGE_LIST
 
 export const DEFAULT_LIST_OF_LISTS: string[] = [
-  AB_TOKEN_LIST_URL,
   TOP_15_TOKEN_List,
+  AEB_TOKENLIST,
   DEFAULT_TOKEN_LIST_URL,
   DEFI_TOKEN_LIST,
   STABLECOIN_TOKEN_LIST,
+  AVAX_BRIDGE_LIST,
   FUJI_TOKEN_LIST_URL
 ]
